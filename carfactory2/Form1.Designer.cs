b0ab689413a451939b6896694fa434ca89556123
danysss <kozakpl11@interia.pl>
2017-10-28 16:16:42 +0200

    wersja 8 z desperacja

--- carfactory2/Form1.Designer.cs
+++ carfactory2/Form1.Designer.cs
@@ -33,7 +33,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(328, 197);
+            this.button1.Location = new System.Drawing.Point(456, 270);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(97, 23);
             this.button1.TabIndex = 0;
