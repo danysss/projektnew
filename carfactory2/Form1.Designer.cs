9d58eb436b8088cbd6571aa56577bc424986e979
danysss <kozakpl11@interia.pl>
2017-10-28 16:21:28 +0200

    zmiana nr 10 testowa

--- carfactory2/Form1.Designer.cs
+++ carfactory2/Form1.Designer.cs
@@ -33,11 +33,11 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(456, 270);
+            this.button1.Location = new System.Drawing.Point(540, 304);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(97, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "PrzyciskTibijski";
+            this.button1.Text = "Przycisk Testowy";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
